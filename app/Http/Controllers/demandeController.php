c36e04b194d149f2dfc0c4f50c916cb8709e216e
MouadK <cream@colorelephant.com>
2021-01-23 22:41:32 +0100

    Added demandes and type de sangs requests

--- app/Http/Controllers/demandeController.php
+++ app/Http/Controllers/demandeController.php
@@ -12,14 +12,11 @@ class DemandeController extends Controller
     public function add(Request $request)
     {
         $this->validate($request, [
-            'adress' => 'required',
-            'stat' => 'required',
             'ville_id' => 'required|exists:villes,id',
             'type_sang_id' => 'required|exists:type_sangs,id',
         ]);
 
         $demande = new Demande([
-            'stat'   => $request->get('stat'),
             'id_user' => Auth::id(),
             'id_ville' => $request->get('ville_id'),
             'id_type_sang' => $request->get('type_sang_id'),
@@ -30,9 +27,28 @@ class DemandeController extends Controller
         return response($demande);
     }
 
+    public function getAll()
+    {
+        $user = Auth::user();
+
+        if ($user->type_sang_id != null)
+            $demandes = DB::table('demandes')->where('id_type_sang', $user->type_sang_id)->get();
+        else
+            $demandes = DB::table('demandes')->get();
+
+        return response($demandes);
+    }
+
     public function showList($id_user)
     {
         $demande = DB::table('demandes')->where('id_user', $id_user)->get();
+
+        return response($demande);
+    }
+
+    public function mesDemandes()
+    {
+        $demande = DB::table('demandes')->where('id_user', Auth::id())->get();
 
         return response($demande);
     }
--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,37 @@ use App\Http\Controllers\VilleController;
 
 Route::group(['middleware' => ['jwt.verify']], function () {
     Route::get('/user', [UserController::class, 'getAuthenticatedUser']);
+
+
+    //demandes api
+    Route::post('demande', [DemandeController::class, 'add']);
+    Route::get('mes-demandes', [DemandeController::class, 'mesDemandes']);
+    Route::get('demandes', [DemandeController::class, 'getAll']);
+    Route::get('demandes/user/{id_user}', [DemandeController::class, 'showList']);
+    Route::get('demande/{id_dem}', [DemandeController::class, 'getDemandeById']);
+    Route::put('demande/{id_dem}', [DemandeController::class, 'update']);
+    Route::post('demande/{id_dem}', [DemandeController::class, 'confirmDemande']);
+
+    //dons api
+    Route::get('/dons/next/{id_user}', [DonController::class, 'timeUntilNextDon']);
+    Route::get('/dons/stats/year/{id_user}', [DonController::class, 'showDonbyYear']);
+    Route::get('/dons/stats/{id_user}', [DonController::class, 'showStats']);
+    Route::get('/dons/{id_user}', [DonController::class, 'showUserDons']);
+    Route::get('dons/don/{id}', [DonController::class, 'show']);
+    Route::apiResource('dons', DonController::class)->except(['show']);
+
+    //centres api
+    Route::resource('centres', CentreController::class);
+    Route::get('/centre/{ville_id}', [CentreController::class, 'getCentreByVilleId']);
+
+    //villes api
+    Route::resource('villes', VilleController::class);
+
+    //RDV api
+    Route::apiResource('rdv', RdvController::class);
+
+    // type de sang API
+    Route::get('type-sangs', [UserController::class, 'getTypeSangs']);
 });
 
 Route::post('/register', [UserController::class, 'register']);
@@ -39,31 +70,6 @@ Route::post('/change-password', [ChangePasswordController::class, 'passwordReset
 Route::delete('/user/{id}', [UserController::class, 'deleteUser']);
 Route::post('/user/{id}', [UserController::class, 'updateUser']);
 Route::get('/users', [UserController::class, 'allUsers']);
-
-//demandes api
-Route::post('demande', [DemandeController::class, 'add']);
-Route::get('demandes/user/{id_user}', [DemandeController::class, 'showList']);
-Route::get('demande/{id_dem}', [DemandeController::class, 'getDemandeById']);
-Route::put('demande/{id_dem}', [DemandeController::class, 'update']);
-Route::post('demande/{id_dem}', [DemandeController::class, 'confirmDemande']);
-
-//dons api
-Route::get('/dons/next/{id_user}', [DonController::class, 'timeUntilNextDon']);
-Route::get('/dons/stats/year/{id_user}', [DonController::class, 'showDonbyYear']);
-Route::get('/dons/stats/{id_user}', [DonController::class, 'showStats']);
-Route::get('/dons/{id_user}', [DonController::class, 'showUserDons']);
-Route::get('dons/don/{id}', [DonController::class, 'show']);
-Route::apiResource('dons', DonController::class)->except(['show']);
-
-//centres api
-Route::resource('centres', CentreController::class);
-Route::get('/centre/{ville_id}', [CentreController::class, 'getCentreByVilleId']);
-
-//villes api
-Route::resource('villes', VilleController::class);
-
-//RDV api
-Route::apiResource('rdv', RdvController::class);
 
 //Users api
 Route::post('/register', [UserController::class, 'register']);
