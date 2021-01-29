d2759c31962fb8ceae44f7ba32f00ef266708f29
hajarachatibi <69926623+hajarachatibi@users.noreply.github.com>
2021-01-29 01:49:55 +0100

    Update Chart.php

--- app/Widgets/Chart.php
+++ app/Widgets/Chart.php
@@ -6,17 +6,7 @@ use Arrilot\Widgets\AbstractWidget;
 
 class Chart extends AbstractWidget
 {
-    /**
-     * The configuration array.
-     *
-     * @var array
-     */
     protected $config = [];
-
-    /**
-     * Treat this method as a controller action.
-     * Return view() or other content to display.
-     */
     public function run()
     {
         return view('widgets.chart', array_merge($this->config,[ ]));
